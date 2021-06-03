3aee1d07b6a6945d357969295269aa8b380e4b1d
katsumi-watanabe <katsumi_1124@icloud.com>
2021-06-03 05:41:19 +0000

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   get '/' => 'homes#top'
   get 'books' => 'books#index'
-  post 'books/:id'=> 'books#create', as: 'create_book'
+  post 'books' => 'books#create', as: 'create_book'
   get 'books/:id' => 'books#show', as: 'show_book'
-  get 'books/:id' => 'books#edit', as: 'edit_book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
