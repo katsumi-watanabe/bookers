3514a9a864797a989e5e65db035a46d8409f0957
katsumi-watanabe <katsumi_1124@icloud.com>
2021-06-03 09:00:24 +0000

    Third

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,13 @@
 class BooksController < ApplicationController
 
+  def create
+    book = Book.new(book_params)
+    if book.save
+    flash[:notice] = "Book was successfully created."
+    redirect_to show_book_path(book.id)
+  end
+  end
+
   def index
     @book = Book.new
     @books = Book.all
@@ -9,12 +17,6 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-  def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to create_book_path(book.id)
-  end
-
   def edit
     @book = Book.find(params[:id])
   end
@@ -22,7 +24,10 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to "/books"
+    if book.save
+    flash[:notice] = "Book was successfully created."
+    redirect_to show_book_path(book.id)
+  end
   end
 
   def destroy
